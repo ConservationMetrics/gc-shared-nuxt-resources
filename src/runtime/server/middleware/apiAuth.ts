751c23cb4251eee3a36e7ad9bdbce7747eec6ee1
Rudo Kemper <rtakemper@gmail.com>
2024-09-17 08:48:34 -0400

    Fix formatting for useRunTimeConfig to bypass type error

--- src/runtime/server/middleware/apiAuth.ts
+++ src/runtime/server/middleware/apiAuth.ts
@@ -1,10 +1,9 @@
 import { defineEventHandler, H3Event } from "h3";
 import apiRequestValidator from "../../utils/apiRequestValidator";
 
+// prettier-ignore
 // @ts-ignore
-const {
-  public: { appApiKey },
-} = useRuntimeConfig();
+const { public: { appApiKey }} = useRuntimeConfig();
 
 export default defineEventHandler((event: H3Event) => {
   apiRequestValidator(appApiKey)(event);
