8667725b659050df919f79978642f0e2483d0ff4
Rudo Kemper <rtakemper@gmail.com>
2024-09-17 09:24:37 -0400

    Format piled ts-ignore & prettier-ignore comments

--- src/runtime/server/middleware/apiAuth.ts
+++ src/runtime/server/middleware/apiAuth.ts
@@ -1,8 +1,7 @@
 import { defineEventHandler, H3Event } from "h3";
 import apiRequestValidator from "../../../utils/apiRequestValidator";
 
-// prettier-ignore to avoid breaking formatting for @ts-ignore
-// @ts-ignore to avoid type error on useRuntimeConfig, which will be available at runtime in the app
+// @ts-ignore to avoid type error on useRuntimeConfig, which will be available at runtime in the app // prettier-ignore
 const {
   public: { appApiKey },
 } = useRuntimeConfig();
