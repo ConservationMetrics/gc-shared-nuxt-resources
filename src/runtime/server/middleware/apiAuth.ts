cc3640ce9c559118c3eacafc2d9440315a86ba00
Rudo Kemper <rtakemper@gmail.com>
2024-09-17 10:20:03 -0400

    Expect appApiKey as method param instead of getting from runtimeConfig

--- src/runtime/server/middleware/apiAuth.ts
+++ src/runtime/server/middleware/apiAuth.ts
@@ -1,34 +1,31 @@
 import { defineEventHandler, H3Event, createError, eventHandler } from "h3";
 
-// @ts-ignore to avoid type error on useRuntimeConfig, which will be available at runtime in the app
-// prettier-ignore
-const { public: { appApiKey }} = useRuntimeConfig();
+export default (appApiKey: string) =>
+  defineEventHandler(
+    eventHandler((event: H3Event) => {
+      const url = event.node.req.url;
+      if (!url) {
+        return;
+      }
 
-export default defineEventHandler(
-  eventHandler((event: H3Event) => {
-    const url = event.node.req.url;
-    if (!url) {
-      return;
-    }
+      // Only apply middleware to API routes
+      if (!url.startsWith("/api/")) {
+        return;
+      }
 
-    // Only apply middleware to API routes
-    if (!url.startsWith("/api/")) {
-      return;
-    }
+      // Bypass middleware for specific paths
+      if (url.startsWith("/api/map") || url.startsWith("/api/_auth/")) {
+        return;
+      }
 
-    // Bypass middleware for specific paths
-    if (url.startsWith("/api/map") || url.startsWith("/api/_auth/")) {
-      return;
-    }
+      // Match the API key from the request with the app's API key
+      const requestApiKey = event.node.req.headers["x-api-key"];
 
-    // Match the API key from the request with the app's API key
-    const requestApiKey = event.node.req.headers["x-api-key"];
-
-    if (requestApiKey !== appApiKey) {
-      throw createError({
-        status: 403,
-        message: "Forbidden",
-      });
-    }
-  }),
-);
+      if (requestApiKey !== appApiKey) {
+        throw createError({
+          status: 403,
+          message: "Forbidden",
+        });
+      }
+    }),
+  );
